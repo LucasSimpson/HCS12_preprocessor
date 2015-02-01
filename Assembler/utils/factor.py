92bbb014a3a12708db5d3758e5cc07e684c43217
Lucas <lucassimpson05@gmail.com>
2015-02-01 02:04:33 -0500

    long loop actually loops correctly now

--- Assembler/utils/factor.py
+++ Assembler/utils/factor.py
@@ -1,6 +1,21 @@
 import math
 
+
 def factor (num_):
+	def check (a_, b_):
+		c = num - a_ * b_
+		return c > 0 and c <= 127
+
 	num = int (num_)
-	start = int (math.sqrt (num)) + 1
-	return [100, 100, 0] # temp lol
+	a = int (math.sqrt (num)) - 2
+	b = a
+	while (not check (a, b)):
+		if num - a * b > 127:
+			a += 1
+			a, b = b, a
+		if a > num_:
+			print 'broken'
+			return None
+
+	return [a, b, num - a * b]
+
